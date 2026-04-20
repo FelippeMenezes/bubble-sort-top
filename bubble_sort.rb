ece86697fe3f7bc0a7cebf55625a0a7ddd32c36d
Felippe Santana <felippemenezes1982@gmail.com>
2026-04-20 10:26:10 -0300

    Added left_number and right number variabel with if condicional

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,12 +1,20 @@
 def bubble_sort(array)
-  p array
   n = array.length
   (n - 1).times do
-    array.each_with_index do | value, index |
 
+    array.each_with_index do | value, index |
+      left_number = array[index]
+      right_number = array[index + 1]
+
+      if right_number != nil && left_number > right_number
+        array[index + 1] = left_number
+        array[index] = right_number
+      end
     end
+    puts counter += 1
   end
   p array
+  return array
 end
 
 bubble_sort([4,3,78,2,0,2])
